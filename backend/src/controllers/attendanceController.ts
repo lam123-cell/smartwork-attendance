0c893f132b4df9025d03a40708761fc580a89c70
Nguyen Nhat Lam <2251120222@ut.edu.vn>
2025-12-23 19:20:16 +0700

    test

--- backend/src/controllers/attendanceController.ts
+++ backend/src/controllers/attendanceController.ts
@@ -13,8 +13,8 @@ const isCheckInAllowed = () => {
   const now = new Date();
   const vnHour = (now.getUTCHours() + 7) % 24; // Giờ Việt Nam (UTC+7)
   const vnMinute = now.getUTCMinutes();
-  if (vnHour > 9) return false;
-  if (vnHour === 9 && vnMinute > 30) return false;
+  if (vnHour > 21) return false;
+  if (vnHour === 21 && vnMinute > 30) return false;
   return true;
 };
 
